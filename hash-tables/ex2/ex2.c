2c7629fd5812b9f25bdc58e969ed6d7def1697f1
lajawanti <myemail>
2019-02-22 13:26:29 -0500

    ex2.c segmentation error

--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -17,9 +17,21 @@ char **reconstruct_trip(Ticket **tickets, int length)
       char **route = malloc(length * sizeof(char *));
 
       // YOUR CODE HERE
-      //void hash_table_insert(HashTable *ht, char *key, char *value)
-      
-
+      for(int i = 0; i < length; i++) {
+          //void hash_table_insert(HashTable *ht, char *key, char *value)
+          hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+          
+          /* hash-table   :  key  : value
+                             NONE : PDX
+                             PDX  : DCA
+                             DCA  : NONE */
+      }
+      char start_source = "NONE";
+      for(int i = 0; i < length; i++) {
+            route[i] = hash_table_retrieve(ht, start_source);
+            //destination becomes source again..
+            start_source = route[i];
+      }
       return route;
 }
 
@@ -37,25 +49,23 @@ void print_route(char **route, int length)
 int main(void)
 {
   // Short test
+  //tickets an array of Ticket struct type to hold 3 struct with source : desti..
   Ticket **tickets = malloc(3 * sizeof(Ticket *));
 
   Ticket *ticket_1 = malloc(sizeof(Ticket));
-  // NONE : PDX
   ticket_1->source = "NONE";
   ticket_1->destination = "PDX";
-  tickets[0] = ticket_1;
+  tickets[0] = ticket_1;   // NONE : PDX
 
-  //PDX : DCA
   Ticket *ticket_2 = malloc(sizeof(Ticket));
   ticket_2->source = "PDX";
   ticket_2->destination = "DCA";
-  tickets[1] = ticket_2;
+  tickets[1] = ticket_2;   //PDX : DCA
 
-  //DCA : NONE
   Ticket *ticket_3 = malloc(sizeof(Ticket));
   ticket_3->source = "DCA";
   ticket_3->destination = "NONE";
-  tickets[2] = ticket_3;
+  tickets[2] = ticket_3;   //DCA : NONE
 
   print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
 
